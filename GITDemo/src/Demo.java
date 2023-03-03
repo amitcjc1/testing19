6744623941522458e1757f6713b7829e8f85f6d4
amit <amitcjcpune@gmail.com>
2023-03-03 17:52:37 +0530

    by CJC

--- GITDemo/src/Demo.java
+++ GITDemo/src/Demo.java
@@ -2,7 +2,19 @@
 public class Demo 
 {
 
-	public static void main(String[] args) {
-		System.out.println("Hello");
+	public void m1()
+	{
+		System.out.println("Hello m1");
 	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+	Demo d=new Demo();
+	d.m1();
+			
+	}
+	
+	
+	
 }
